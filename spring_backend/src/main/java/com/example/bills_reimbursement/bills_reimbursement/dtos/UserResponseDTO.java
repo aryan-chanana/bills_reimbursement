d82814031922d3ec1365a9da67a499c63fcfcd6f
aryan <aryanchanana1111@gmail.com>
2025-10-14 22:12:26 +0530

    fixing userResponseDTO

--- spring_backend/src/main/java/com/example/bills_reimbursement/bills_reimbursement/dtos/UserResponseDTO.java
+++ spring_backend/src/main/java/com/example/bills_reimbursement/bills_reimbursement/dtos/UserResponseDTO.java
@@ -1,14 +1,8 @@
 package com.example.bills_reimbursement.bills_reimbursement.dtos;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 public class UserResponseDTO {
     private Integer employeeId;
 
