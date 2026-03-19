7c9626d293287627e2a8756d0c948550a4dbc129
aryan <aryanchanana1111@gmail.com>
2026-03-19 12:28:17 +0530

    fixing cors backend

--- spring_backend/src/main/java/com/example/bills_reimbursement/bills_reimbursement/configs/SecurityConfig.java
+++ spring_backend/src/main/java/com/example/bills_reimbursement/bills_reimbursement/configs/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
